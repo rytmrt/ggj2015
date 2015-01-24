87108d390aafab88256cf939f1d1132ee5065bca
MatsuuraAkase <roi.akase@outlook.jp>
2015-01-24 15:24:03 +0900

    キャラクターのプレファブを入れました

--- Assets/Scripts/Main/Character_AI.cs
+++ Assets/Scripts/Main/Character_AI.cs
@@ -9,18 +9,22 @@ public class Character_AI : MonoBehaviour
 {
 	private const float RUN_SPEED = 5.0f;
 	private const float WALK_SPEED = 2.0f;
+
 	public bool isRun;
 	public GameObject[] point = new GameObject[3];
 
+	private bool isMove;
 	private int next_point;
 	private int length;
 	private int plus;
 	private float speed;
 
-	void Start () 
+	private IEnumerator Start () 
 	{
+		yield return new WaitForSeconds(1.0f);
 		transform.position = point[0].transform.position;
 		plus = 1;
+		isMove = true;
 		next_point = 1;
 		speed = (isRun) ? RUN_SPEED : WALK_SPEED;
 		length = point.Length;
@@ -28,11 +32,14 @@ public class Character_AI : MonoBehaviour
 
 	void Update ()
 	{
-		var vec = (point[next_point].transform.position - transform.position).normalized;
-		transform.position += vec * speed * Time.deltaTime;
-		var dis = Vector3.Distance(transform.position, point[next_point].transform.position);
-		if (dis < 0.5f) { Next_Point_Go(); }
-		Debug.Log(next_point);
+		if (isMove)
+		{
+			var vec = (point[next_point].transform.position - transform.position).normalized;
+			transform.position += vec * speed * Time.deltaTime;
+			var dis = Vector3.Distance(transform.position, point[next_point].transform.position);
+			if (dis < 0.5f) { Next_Point_Go(); }
+			Debug.Log(next_point);
+		}
 	}
 
 	/// <summary>
@@ -44,6 +51,17 @@ public class Character_AI : MonoBehaviour
 			plus = -plus;
 		next_point += plus;
 	}
+
+	/// <summary>
+	/// キャラが穴に落ちる
+	/// </summary>
+	void Fall()
+	{
+		isMove = false;
+		GetComponent<Collider>().enabled = false;
+	}
+
+	
 
 
 }
