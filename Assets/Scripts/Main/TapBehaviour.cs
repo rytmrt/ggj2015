dc348a6a4eef9c9c35ddecf6790d651ba766d514
MatsuuraAkase <roi.akase@outlook.jp>
2015-01-24 18:29:57 +0900

    タッチするとRayを発射してオブジェクトを判定するようにしました

---
+++ Assets/Scripts/Main/TapBehaviour.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class TapBehaviour : MonoBehaviour {
+    // タッチしたときに呼ばれる。
+    public virtual void TapDown(ref RaycastHit hit){}
+    // タッチを離したときに呼ばれる。
+    public virtual void TapUp(ref RaycastHit hit){}
+}
---
+++ Assets/Scripts/Main/Tap_Event.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tap_Event : TapBehaviour {
+
+	public override void TapDown(ref RaycastHit hit)
+	{
+		Debug.Log("tapdown");
+		// タップされたときの処理
+	}
+
+	public override void TapUp(ref RaycastHit hit)
+	{
+		// タップを離したときの処理
+	}
+}
