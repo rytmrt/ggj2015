357194414220ea1b19fd17f6ba9de43dd2f37294
SobajimaRyoya <bird_ochestra-requiem@yahoo.co.jp>
2015-01-24 17:31:22 +0900

    神様のレベルが上がりました。落とし穴が消せるように。

---
+++ Assets/01Scripts/Gimmick/Fall.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fall : MonoBehaviour
+{
+    bool isRunning;
+
+	// Use this for initialization
+	void Start () {
+        isRunning = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //ギミックのオン、オフを切り替える
+    void GimmickOn() {
+        isRunning = true;
+        renderer.enabled = true;
+    }
+    void GimmickOff() {
+        isRunning = false;
+        renderer.enabled = false;
+    }
+
+
+
+    void OnTriggerEnter(Collider chara) {
+        Vector3 charaPos = transform.position;
+        if (chara.tag == "Player" && isRunning)
+        {
+            chara.SendMessage("Fall");
+            charaPos.y = chara.transform.position.y;
+            chara.transform.position = charaPos;
+        }
+    }
+}
---
+++ Assets/01Scripts/New Folder/HoleRepair.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoleRepair : MonoBehaviour {
+
+    GameObject[] holes;
+
+    int i = 0;
+
+	// Use this for initialization
+	void Start () {
+        holes = GameObject.FindGameObjectsWithTag("Hole");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+
+
+    public void Repair() {
+/*        if (i < holes.Length)
+        {
+           
+            i++;
+        }*/
+        holes[0].SendMessage("GimmickOff");
+    }
+
+
+}
