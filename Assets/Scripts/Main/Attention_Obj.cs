5b04d9c3eeacf250deece5255265bddf5157340f
MatsuuraAkase <roi.akase@outlook.jp>
2015-01-25 11:44:31 +0900

    ビックリマークのプレファブを作りました

---
+++ Assets/Scripts/Main/Attention_Obj.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attention_Obj : MonoBehaviour {
+
+	void Start ()
+	{
+	
+	}
+	
+	void Update () {
+		transform.rotation = Camera.main.transform.rotation;
+	}
+}
